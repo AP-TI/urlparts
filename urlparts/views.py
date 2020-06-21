deffaf095b7583840f8c6414eaf1e89ff1e88d7d
mixxamm <maximjanssensj5@gmail.com>
2020-06-21 14:37:36 +0200

    Fix foutje
    Als de eerste variabele leeg was, dan zou deze nog worden afgedrukt. Nu slagen alle testen.

--- urlparts/views.py
+++ urlparts/views.py
@@ -5,6 +5,6 @@ import re
 # Create your views here.
 def index(request, path):
     pathvariables = path.split('/')
-    if '//' in path:
+    if '//' in path or pathvariables[0] == '':
         return HttpResponse("Ongeldige url")
     return render(request, 'urlparts/index.html', {'parts': pathvariables})
